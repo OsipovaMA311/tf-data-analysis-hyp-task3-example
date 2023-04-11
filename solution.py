d8c261832477b747aa2d78de40efcb5936e9f2fa
marina_a_o <82184941+OsipovaMA311@users.noreply.github.com>
2023-04-11 19:20:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,19 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import mannwhitneyu
 
+chat_id = 468819439 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    sample1 = x.astype(float)
+    sample1 = sample1 + 1
+    sample1 = sample1.apply(np.log, axis=0)
+    sample2 = y.astype(float)
+    sample2 = sample2 + 1
+    sample2 = sample2.apply(np.log, axis=0)
+    
+    stat, pval = mannwhitneyu(sample1, sample2, alternative='greater')
+    return pval < 0.07 # Ваш ответ, True или False
